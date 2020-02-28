86b6b5a5a5b2a60a0f6b540f76d74bca6addcfc8
Kyle Prifogle <kprifogle1@gmail.com>
2020-02-28 15:52:54 -0500

    Adding mason cli tool

---
+++ python/cli/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+long_description = ''
+
+setup(
+    name='mason',
+    version='1.0.0',
+    author='Kyle Prifogle',
+    author_email='kyle.prifogle@samtec.com',
+    url='',
+    description='',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'mason = mason.cli:main'
+        ]
+    },
+    install_requires=requirements,
+    zip_safe=False
+)
+
