8d550faedb5f73f4eb141647e170a03ea788dbf7
Kyle Prifogle <kprifogle1@gmail.com>
2020-03-12 12:57:41 -0400

    Organizing cli examples

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 set -e
 
-./scripts/install.sh
-rm -rf ~/.mason/
-mason config -l debug
+# Reset everything to 0
+# ./scripts/install.sh
+# rm -rf ~/.mason/
+
+# config examples
+# mason config -l debug
 # mason config examples/config/bad_config.yaml
-mason config examples/config/config_example.yaml
+# mason config examples/config/config_example.yaml
 # mason config
 # mason register
-#mason register examples/operators/table
+# mason register examples/operators/table
 # mason run
+
+# register exampels
 # mason register examples/operators/table/list/  # TODO: PLEASE FIX THIS CASE
- mason register examples/operators/table/
+# mason register examples/operators/table/
+
+# operator examples
 # mason operator
 # mason operator -l debug
 # mason operator table
@@ -19,8 +26,15 @@ mason config examples/config/config_example.yaml
 # mason operator table get -p datebase_name:crawler-poc,bad
 # mason operator table get -p bad
 # mason operator table get -p database_name:crawler-poc,table_name:catalog_poc_data
+
+# list examples
 # mason operator table list
 # mason operator table list -p database_name:crawler-poc
+
+# refresh examples
 # mason operator table refresh -p database_name:crawler-poc,table_name:catalog_poc_data
- mason operator table infer -p schedule_name:crawler-shipment,database_name:crawler-shipment,storage_path:lake-working-copy-feb-20-2020/logistics-bi-data-publisher/prod/shipment/
+
+# infer examples
+# mason operator table infer -p schedule_name:test_crawler,database_name:crawler-poc,storage_path:lake-working-copy-feb-20-2020/user-data/kyle.prifogle/catalog_poc_data/
+# mason operator table infer -p schedule_name:crawler-shipment,database_name:crawler-shipment,storage_path:lake-working-copy-feb-20-2020/logistics-bi-data-publisher/prod/shipment/
 
