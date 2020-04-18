ae571024466e3352202eea5739354a22b88023d9
Kyle Prifogle <kprifogle1@gmail.com>
2020-04-18 00:58:17 -0400

    Finishing up demo script, adding check for metastore credentals

--- demos/run_demo.sh
+++ demos/run_demo.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+trap 'rm -rf ../.tmp/' EXIT
+
 if [ $1 == "1.02" ]
 then
   commands=(
@@ -14,22 +16,25 @@ then
     "mason operator"
     "mason operator table get -p \"database_name:spg-mason-demo,table_name:part_data\""
     "mason operator table merge -p \"input_path:spg-mason-demo/part_data/,output_path:spg-mason-demo/part_data_merged/\""
-    "mason operator table get -p \"database_name:spg-mason-demo,table_name:part_data_merged\""
+    "mason operator table get -p \"database_name:spg-mason-demo,table_name:part_data_csv\""
+    "mason operator table merge -p \"input_path:spg-mason-demo/part_data_csv/,output_path:spg-mason-demo/part_data_csv_merged/\""
+    "mason operator table get -p \"database_name:spg-mason-demo,table_name:part_data_json\""
+    "mason operator table merge -p \"input_path:spg-mason-demo/part_data_json/,output_path:spg-mason-demo/part_data_json_merged/\""
   )
 else
   commands=()
 fi
 
 cd ..
+rm -rf '.tmp/'
 mkdir '.tmp'
 export MASON_HOME='.tmp/'
 
 for i in "${commands[@]}"
 do
-  # echo "Press [Enter] to continue demo:"
-  # read -p "$i"
+  echo "Press [Enter] to continue demo:"
+  read -p "$i"
   eval "$i"
 done
 
-trap 'rm -rf .tmp' EXIT
 
--- examples/operators/table/merge/__init__.py
+++ examples/operators/table/merge/__init__.py
@@ -24,20 +24,23 @@ def run(env: MasonEnvironment, config: Config, parameters: Parameters, response:
     metastore_client = config.metastore.client
     metastore_credentials: MetastoreCredentials = metastore_client.credentials()
 
-    input_path = parameters.safe_get("input_path")
-    output_path =parameters.safe_get("output_path")
-    parse_headers = bool(parameters.unsafe_get("parse_headers"))
-
-    input_database, input_table = metastore_client.parse_path(input_path)
-
-    schemas, response = metastore_client.get_table(input_database, input_table, response, {"read_headers": parse_headers})
-    schema_types: Set[str] = set(map(lambda schema: schema.type, schemas)) # TODO: Ensure this has length one, collapse list to singleton earlier
-
-    if len(schemas) > 0 and schema_types.issubset(SUPPORTED_SCHEMAS):
-        p = {'input_path': input_path, 'output_path': output_path, 'input_format': next(iter(schema_types))}
-        response = config.execution.client.run_job("merge", metastore_credentials, p, response)
+    if not metastore_credentials.secret_key or not metastore_credentials.access_key:
+        response.add_error("Metastore credentials undefined")
     else:
-        response.add_error(f"Unsupported schemas for merge operator: {', '.join(list(schema_types.difference(SUPPORTED_SCHEMAS)))}")
+        input_path = parameters.safe_get("input_path")
+        output_path =parameters.safe_get("output_path")
+        parse_headers = bool(parameters.unsafe_get("parse_headers"))
+
+        input_database, input_table = metastore_client.parse_path(input_path)
+
+        schemas, response = metastore_client.get_table(input_database, input_table, response, {"read_headers": parse_headers})
+        schema_types: Set[str] = set(map(lambda schema: schema.type, schemas)) # TODO: Ensure this has length one, collapse list to singleton earlier
+
+        if len(schemas) > 0 and schema_types.issubset(SUPPORTED_SCHEMAS):
+            p = {'input_path': input_path, 'output_path': output_path, 'input_format': next(iter(schema_types))}
+            response = config.execution.client.run_job("merge", metastore_credentials, p, response)
+        else:
+            response.add_error(f"Unsupported schemas for merge operator: {', '.join(list(schema_types.difference(SUPPORTED_SCHEMAS)))}")
 
     return response
 
