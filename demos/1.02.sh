139850a3a1bbea645bc2abba6d05877e163ef21e
Kyle Prifogle <kprifogle1@gmail.com>
2020-04-21 10:31:29 -0400

    Fixing some issues with the demo

--- demos/1.02.sh
+++ demos/1.02.sh
@@ -6,5 +6,7 @@ echo "Requirements:  docker"
 echo "Press [Enter] to continue demo:"
 read -p "$*"
 
-docker run -it samtecspg/mason:v1.02 /bin/sh demos/run_demo.sh 1.02
+cd ..
+./docker_build
 
+docker run -it --env-file=.env -v=$HOME/.kube/:/app/.kube/ samtecspg/mason:v1.02 ./demos/run_demo.sh 1.02
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 set -e
 
-mypy .
 python3 server.py
