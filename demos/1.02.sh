a2fbb869fe73296d471ec5a9028eaeb2c310eab6
Kyle Prifogle <kprifogle1@gmail.com>
2020-04-21 11:08:18 -0400

    Removing docker build from demo script

--- demos/1.02.sh
+++ demos/1.02.sh
@@ -2,11 +2,10 @@
 #!/bin/bash
 set -e
 
-echo "Requirements:  docker"
+echo "Requirements:  docker.  Run docker_build in parent directory."
 echo "Press [Enter] to continue demo:"
 read -p "$*"
 
 cd ..
-./docker_build
 
 docker run -it --env-file=.env -v=$HOME/.kube/:/app/.kube/ samtecspg/mason:v1.02 ./demos/run_demo.sh 1.02
