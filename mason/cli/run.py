a2cc02479b7969e351ae59e9048d93e6fa919839
Kyle Prifogle <kprifogle1@gmail.com>
2020-06-02 13:29:19 -0400

    First round of import refactors which signifigantly improved CLI execution time

---
+++ mason/cli/run.py
@@ -0,0 +1,12 @@
+import click
+
+class Run:
+    @click.command("run", short_help="Runs mason flask server on port 5000")
+    def run(port: str = 5000):
+        """
+        Will run mason flask server on port 5000.
+        To view the mason swagger ui go to: http://localhost:5000/api/ui/
+        """
+        from mason.server import MasonServer
+        MasonServer().run()
+
