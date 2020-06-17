10dd0dd1ecd755356774e80e78a1480613407c6f
Kyle Prifogle <kprifogle1@gmail.com>
2020-06-17 13:22:01 -0400

    Finishing up test and addressing issues with it

---
+++ mason/test/support/operators/namespace2/operator5/__init__.py
@@ -0,0 +1,10 @@
+from mason.clients.response import Response
+from mason.operators.operator_response import OperatorResponse
+
+from mason.parameters.validated_parameters import ValidatedParameters
+from mason.configurations.valid_config import ValidConfig
+from mason.util.environment import MasonEnvironment
+
+def run(env: MasonEnvironment, config: ValidConfig, parameters: ValidatedParameters, response: Response) -> OperatorResponse:
+    response.add_info("Running operator5")
+    return OperatorResponse(response)
