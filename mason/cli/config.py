db4ff15c3de5c26fb70e8ea7df6cf9f3182a217b
Kyle Prifogle <kprifogle1@gmail.com>
2021-02-19 09:59:41 -0500

    Moving import

--- mason/cli/config.py
+++ mason/cli/config.py
@@ -2,14 +2,13 @@ from typing import Optional
 
 import click
 
-from mason.api.config import config as api_config
-
 @click.command("config", short_help="Interact with mason configs")
 @click.argument("config_id", required=True)
 @click.option("-s", "--set_current", help="Sets config as current session config")
 @click.option("-l", "--log_level", help="Log level for mason")
 
 def config(config_id: str, set_current: bool = False, log_level: Optional[str] = None):
+    from mason.api.config import config as api_config
     """
     Sets mason config for session.  Alias for get:namespace=config_id, with added ability to set session config
     [CONFIG_ID] is a config_id 
